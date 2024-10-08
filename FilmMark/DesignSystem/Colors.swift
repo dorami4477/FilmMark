1ed2078b9a84713ddde41c0d457445757955a719
jeongyun-kim <kkanyo04@naver.com>
2024-10-08 18:12:52 +0900

    design: DesignSystem 초기 세팅

---
+++ FilmMark/DesignSystem/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  FilmMark
+//
+//  Created by 김정윤 on 10/8/24.
+//
+
+import UIKit
+
+enum Colors {
+    static let primaryColor = UIColor(red: 0.49, green: 0.00, blue: 1.00, alpha: 1.00)
+    static let systemGray6: UIColor = .systemGray6
+    static let white: UIColor = .white
+    static let black: UIColor = .black
+}
---
+++ FilmMark/DesignSystem/Icons.swift
@@ -0,0 +1,19 @@
+//
+//  Icons.swift
+//  FilmMark
+//
+//  Created by 김정윤 on 10/8/24.
+//
+
+import UIKit
+
+enum Icons {
+    static let play = UIImage(systemName: "play")
+    static let search = UIImage(systemName: "magnifyingglass")
+    static let tv = UIImage(systemName: "sparkles.tv")
+    static let home = UIImage(systemName: "house")
+    static let playCircle = UIImage(systemName: "play.circle")
+    static let down = UIImage(systemName: "square.and.arrow.down")
+    static let smile = UIImage(systemName: "face.smiling")
+    static let plus = UIImage(systemName: "plus")
+}
